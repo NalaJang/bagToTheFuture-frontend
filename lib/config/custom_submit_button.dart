d48154cd793f82f44f0024a41f0e20b46baba85e
nala0897@naver.com <nala0897@naver.com>
2024-04-14 17:37:47 +0900

    feat: 비밀번호 변경 화면 viewModel 연결 및 코드 정리

--- lib/config/custom_submit_button.dart
+++ lib/config/custom_submit_button.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:rest_api_ex/config/navigate_to.dart';
-import 'package:rest_api_ex/config/validation_check.dart';
-import 'package:rest_api_ex/data/network/error_handler.dart';
 import 'package:rest_api_ex/design/color_styles.dart';
 import 'package:rest_api_ex/design/font_styles.dart';
-import 'package:rest_api_ex/screen/view_model/user_info_view_model.dart';
 
 class CustomSubmitButton extends StatelessWidget {
   final GlobalKey<FormState> formKey;
   final String buttonText;
   final Widget nextPage;
-  final UserInfoViewModel? viewModel;
+  final VoidCallback onPressedCallback;
 
   const CustomSubmitButton({
     super.key,
     required this.formKey,
     required this.buttonText,
     required this.nextPage,
-    this.viewModel,
+    required this.onPressedCallback,
   });
 
   @override
@@ -30,27 +26,7 @@ class CustomSubmitButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(4.0),
         ),
       ),
-      onPressed: () {
-        ValidationCheck().allUserInputValidation(formKey);
-
-        try {
-          viewModel!.submit(password: '12345');
-
-          if (context.mounted) {
-            navigateTo(context, nextPage);
-          }
-        } catch (error) {
-          final errorMessage = ErrorHandler.handle(error).failure;
-
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(errorMessage),
-              ),
-            );
-          }
-        }
-      },
+      onPressed: onPressedCallback,
       child: Text(
         buttonText,
         style: FontStyles.Body2.copyWith(color: AppColors.white),
--- lib/screen/sign_up/reset_pw_screen.dart
+++ lib/screen/sign_up/reset_pw_screen.dart
@@ -10,113 +10,111 @@ import 'package:rest_api_ex/config/validation_check.dart';
 import 'package:rest_api_ex/design/color_styles.dart';
 import 'package:rest_api_ex/design/font_styles.dart';
 import 'package:rest_api_ex/screen/my_bottom_navigation.dart';
-import 'package:rest_api_ex/screen/view_model/user_info_view_model.dart';
+import 'package:rest_api_ex/screen/view_model/reset_pw_view_model.dart';
 
-class ResetPwScreen extends StatefulWidget {
+class ResetPwScreen extends StatelessWidget {
   const ResetPwScreen({super.key});
 
   @override
-  State<ResetPwScreen> createState() => _ResetPwScreenState();
-}
-
-class _ResetPwScreenState extends State<ResetPwScreen> {
-  bool showSpinner = false;
-  final formKey = GlobalKey<FormState>();
-  final _userPasswordController = TextEditingController();
-  final _userPasswordConfirmController = TextEditingController();
-
-  @override
-  void dispose() {
-    _userPasswordController.dispose();
-    _userPasswordConfirmController.dispose();
-
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final viewModel = context.watch<UserInfoViewModel>();
+    return ChangeNotifierProvider(
+      create: (_) => ResetPwViewModel(),
+      builder: (context, child) {
+        final viewModel = context.watch<ResetPwViewModel>();
 
-    return Scaffold(
-      appBar: const CustomAppBar(title: Constants.resetPw),
-      body: ModalProgressHUD(
-        inAsyncCall: viewModel.isLoading,
-        child: Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  Constants.resetPwDescription,
-                  style: FontStyles.Title2.copyWith(
-                    color: AppColors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                SizedBoxValues.gapH20,
-
-                Row(
+        return Scaffold(
+          appBar: const CustomAppBar(title: Constants.resetPw),
+          body: ModalProgressHUD(
+            inAsyncCall: viewModel.showSpinner,
+            child: Form(
+              key: viewModel.formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    const Text(
-                      Constants.password,
-                      style: FontStyles.Body2,
-                    ),
-                    Text(
-                      Constants.asterisk,
-                      style: FontStyles.Body2.copyWith(color: AppColors.red),
-                    )
+                    _description(),
+                    SizedBoxValues.gapH20,
+                    _inputPassword(viewModel),
+                    SizedBoxValues.gapH20,
+                    _changePassword(viewModel, context),
                   ],
                 ),
-
-                // 비밀번호
-                UserInfoTextFormField(
-                  controller: _userPasswordController,
-                  validator: validatePassword,
-                  decorationLabelText: Constants.pwValidationRule,
-                ),
-
-                SizedBoxValues.gapH20,
-
-                Row(
-                  children: [
-                    const Text(
-                      Constants.checkPassword,
-                      style: FontStyles.Body2,
-                    ),
-                    Text(
-                      Constants.asterisk,
-                      style: FontStyles.Body2.copyWith(color: AppColors.red),
-                    )
-                  ],
-                ),
-
-                // 비밀번호 확인
-                UserInfoTextFormField(
-                  controller: _userPasswordConfirmController,
-                  validator: (value) => validateConfirmPassword(
-                    value,
-                    _userPasswordController.text,
-                  ),
-                  decorationLabelText: Constants.pwValidationRule,
-                ),
-
-                SizedBoxValues.gapH20,
-
-                // 비밀번호 변경 버튼
-                CustomSubmitButton(
-                  formKey: formKey,
-                  buttonText: Constants.submitNewPw,
-                  nextPage: const MyBottomNavigation(),
-                  viewModel: viewModel,
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        );
+      },
+    );
+  }
+
+  Widget _description() {
+    return Text(
+      Constants.resetPwDescription,
+      style: FontStyles.Title2.copyWith(
+        color: AppColors.black,
+        fontWeight: FontWeight.bold,
       ),
+    );
+  }
+
+  Widget _inputPassword(ResetPwViewModel viewModel) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text(
+              Constants.password,
+              style: FontStyles.Body2,
+            ),
+            Text(
+              Constants.asterisk,
+              style: FontStyles.Body2.copyWith(color: AppColors.red),
+            )
+          ],
+        ),
+
+        // 비밀번호
+        UserInfoTextFormField(
+          controller: viewModel.userPasswordController,
+          validator: validatePassword,
+          decorationLabelText: Constants.pwValidationRule,
+        ),
+
+        SizedBoxValues.gapH20,
+
+        Row(
+          children: [
+            const Text(
+              Constants.checkPassword,
+              style: FontStyles.Body2,
+            ),
+            Text(
+              Constants.asterisk,
+              style: FontStyles.Body2.copyWith(color: AppColors.red),
+            )
+          ],
+        ),
+
+        // 비밀번호 확인
+        UserInfoTextFormField(
+          controller: viewModel.userPasswordConfirmController,
+          validator: (value) => validateConfirmPassword(
+            value,
+            viewModel.userPasswordController.text,
+          ),
+          decorationLabelText: Constants.pwValidationRule,
+        ),
+      ],
+    );
+  }
+
+  Widget _changePassword(ResetPwViewModel viewModel, BuildContext context) {
+    return CustomSubmitButton(
+      formKey: viewModel.formKey,
+      buttonText: Constants.submitNewPw,
+      nextPage: const MyBottomNavigation(),
+      onPressedCallback: () => viewModel.submitNewPassword(context, 'password'),
     );
   }
 }

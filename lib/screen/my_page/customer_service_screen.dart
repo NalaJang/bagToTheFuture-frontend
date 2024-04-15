0e29a8150ac143336b8f4433a29ad77db25168ed
nala0897@naver.com <nala0897@naver.com>
2024-04-16 07:57:14 +0900

    feat: 링크 클릭 시 카카오톡 채널 웹 페이지로 이동

--- lib/screen/my_page/customer_service_screen.dart
+++ lib/screen/my_page/customer_service_screen.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rest_api_ex/data/model/qna_model.dart';
 import 'package:rest_api_ex/data/source/qna_data.dart';
 import 'package:rest_api_ex/design/color_styles.dart';
 import 'package:rest_api_ex/design/font_styles.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomerServiceScreen extends StatelessWidget {
   final List<QNAModel> qnaList = QNAData.qnaData;
@@ -24,9 +27,25 @@ class CustomerServiceScreen extends StatelessWidget {
             ),
           ),
           _qnaList(),
+          ListTile(
+            title: Text('기타 문의사항이 있으신가요?'),
+            subtitle: Text('백투더퓨처 상담 채널 연결'),
+            trailing: IconButton(
+              icon: Icon(CupertinoIcons.forward),
+              onPressed: _toKakaoChannel,
+            ),
+          )
         ],
       ),
     );
+  }
+
+  Future<void> _toKakaoChannel() async {
+    final Uri url = Uri.parse(dotenv.env['KAKAO_CHANNEL_URL']!);
+
+    if (!await launchUrl(url)) {
+      debugPrint('Could not launch $url');
+    }
   }
 
   Widget _qnaList() {
@@ -36,41 +55,47 @@ class CustomerServiceScreen extends StatelessWidget {
             (qna) => ExpansionPanelRadio(
               canTapOnHeader: true,
               value: qna,
-              headerBuilder: (context, isExpanded) => Row(
-                children: [
-                  Text(
-                    'Q',
-                    style: FontStyles.Body1.copyWith(
-                      color: AppColors.main,
+              headerBuilder: (context, isExpanded) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Q ',
+                      style: FontStyles.Body1.copyWith(
+                        color: AppColors.main,
+                      ),
                     ),
-                  ),
-                  Text(
-                    qna.question,
-                    style: FontStyles.Body7.copyWith(
-                      color: AppColors.black,
+                    Text(
+                      qna.question,
+                      style: FontStyles.Body7.copyWith(
+                        color: AppColors.black,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              body: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'A',
-                    style: FontStyles.Body1.copyWith(
-                      color: AppColors.gray5,
+              body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'A ',
+                      style: FontStyles.Body1.copyWith(
+                        color: AppColors.gray5,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      qna.answer,
-                      // overflow: TextOverflow.clip,
-                      // maxLines: 3,
-                      style: FontStyles.Body7.copyWith(color: AppColors.black),
+                    Expanded(
+                      child: Text(
+                        qna.answer,
+                        // overflow: TextOverflow.clip,
+                        // maxLines: 3,
+                        style: FontStyles.Body7.copyWith(color: AppColors.black),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           )

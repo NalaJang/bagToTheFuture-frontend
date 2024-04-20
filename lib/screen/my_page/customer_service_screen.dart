6e8f794bbd64549a4c55b0520dabfe9f38f88f84
nala0897@naver.com <nala0897@naver.com>
2024-04-21 08:52:11 +0900

    feat: 서비스 피드백 메뉴 추가, 카카오 채널 연결, 고객센터 UI 전체 수정

--- lib/screen/my_page/customer_service_screen.dart
+++ lib/screen/my_page/customer_service_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rest_api_ex/config/constants.dart';
@@ -10,10 +9,23 @@ import 'package:rest_api_ex/design/font_styles.dart';
 import 'package:rest_api_ex/design/svg_icon.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class CustomerServiceScreen extends StatelessWidget {
+class CustomerServiceScreen extends StatefulWidget {
+  const CustomerServiceScreen({super.key});
+
+  @override
+  State<CustomerServiceScreen> createState() => _CustomerServiceScreenState();
+}
+
+class _CustomerServiceScreenState extends State<CustomerServiceScreen> {
   final List<QNAModel> qnaList = QNAData.qnaData;
 
-  const CustomerServiceScreen({super.key});
+  @override
+  void dispose() {
+    for(int i = 0; i < qnaList.length; i++) {
+      qnaList[i].isExpanded = false;
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,93 +33,141 @@ class CustomerServiceScreen extends StatelessWidget {
       appBar: const CustomAppBar(
         title: Constants.customerService,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Text(
-              '자주 묻는 질문',
-              style: FontStyles.Body1.copyWith(
-                color: AppColors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0, left: 18.0),
+                child: Text(
+                  '자주 묻는 질문',
+                  style: FontStyles.Body1.copyWith(
+                    color: AppColors.black,
+                  ),
+                ),
               ),
-            ),
+              const Divider(),
+              _qnaList(),
+              _qnaChannel(),
+              _serviceFeedback(),
+            ],
           ),
-          Divider(),
-          _qnaList(),
-          _qnaChannel(),
-        ],
+        ),
       ),
     );
   }
 
   Widget _qnaList() {
-    return ExpansionPanelList.radio(
-      children: qnaList
-          .map(
-            (qna) => ExpansionPanelRadio(
-              canTapOnHeader: true,
-              value: qna,
-              headerBuilder: (context, isExpanded) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'Q ',
-                      style: FontStyles.Body1.copyWith(
-                        color: AppColors.main,
-                      ),
-                    ),
-                    Text(
-                      qna.question,
-                      style: FontStyles.Body7.copyWith(
-                        color: AppColors.black,
-                      ),
-                    ),
-                  ],
+    return ListView.builder(
+      primary: false,
+      shrinkWrap: true,
+      itemCount: qnaList.length,
+      itemBuilder: (context, index) {
+        return ExpansionTile(
+          trailing: qnaList[index].isExpanded
+              ? SvgIcon.arrowUp(color: AppColors.main)
+              : SvgIcon.arrowDown(color: AppColors.main),
+          onExpansionChanged: (bool expanded) {
+            setState(() {
+              qnaList[index].isExpanded = expanded;
+            });
+          },
+          tilePadding: const EdgeInsets.only(left: 12.0, right: 16.0),
+          title: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 9.0),
+                child: Text(
+                  'Q',
+                  style: FontStyles.Body1.copyWith(
+                    color: AppColors.main,
+                  ),
                 ),
               ),
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'A ',
+              Text(
+                qnaList[index].question,
+                style: FontStyles.Body7.copyWith(color: AppColors.black),
+              ),
+            ],
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 21.0,
+                bottom: 18.0,
+                left: 12.0,
+                right: 16.0,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 9.0),
+                    child: Text(
+                      'A',
                       style: FontStyles.Body1.copyWith(
                         color: AppColors.gray5,
                       ),
                     ),
-                    Expanded(
-                      child: Text(
-                        qna.answer,
-                        // overflow: TextOverflow.clip,
-                        // maxLines: 3,
-                        style:
-                            FontStyles.Body7.copyWith(color: AppColors.black),
+                  ),
+                  Expanded(
+                    child: Text(
+                      qnaList[index].answer,
+                      // overflow: TextOverflow.clip,
+                      // maxLines: 3,
+                      style: FontStyles.Body7.copyWith(
+                        color: AppColors.black,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          )
-          .toList(),
+          ],
+        );
+      },
     );
   }
 
   Widget _qnaChannel() {
-    return ListTile(
-      leading: SvgIcon.headPhone(color: AppColors.black),
-      title: Text('기타 문의사항이 있으신가요?', style: FontStyles.Caption2,),
-      subtitle: Text('백투더퓨처 상담 채널 연결', style: FontStyles.Body1,),
-      trailing: IconButton(
-        icon: const Icon(CupertinoIcons.forward, color: AppColors.gray4,),
-        onPressed: _toKakaoChannel,
-      ),
-      contentPadding: EdgeInsets.only(left: 8.0, right: 8.0),
+    return InkWell(
+      onTap: () => _toKakaoChannel(),
 
+      child: ListTile(
+        contentPadding: const EdgeInsets.only(top: 21.0, left: 19.0, right: 18.0),
+        leading: SvgIcon.headPhone(color: AppColors.black),
+        title: const Text(
+          '기타 문의사항이 있으신가요?',
+          style: FontStyles.Caption2
+        ),
+        subtitle: const Text(
+          '백투더퓨처 상담 채널 연결',
+          style: FontStyles.Body1,
+        ),
+        trailing: SvgIcon.arrowRight(color: AppColors.gray4),
+      ),
+    );
+  }
+
+  Widget _serviceFeedback() {
+    return InkWell(
+      onTap: () => _toKakaoChannel(),
+
+      child: ListTile(
+        contentPadding: const EdgeInsets.only(top: 12.0, left: 19.0, right: 18.0),
+        leading: SvgIcon.serviceFeedback(color: AppColors.black),
+        title: const Text(
+          '백투퓨처의 개선점을 보내주세요!',
+          style: FontStyles.Caption2,
+        ),
+        subtitle: const Text(
+          '서비스 피드백',
+          style: FontStyles.Body1,
+        ),
+        trailing: SvgIcon.arrowRight(color: AppColors.gray4),
+      ),
     );
   }
 
@@ -118,5 +178,4 @@ class CustomerServiceScreen extends StatelessWidget {
       debugPrint('Could not launch $url');
     }
   }
-
 }

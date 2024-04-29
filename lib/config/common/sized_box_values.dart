573ef848028dd4dff00dca132f595163b507c406
nala0897@naver.com <nala0897@naver.com>
2024-04-29 19:42:38 +0900

    feat: 환불 정보 화면 UI

--- lib/config/common/sized_box_values.dart
+++ lib/config/common/sized_box_values.dart
@@ -9,7 +9,9 @@ class SizedBoxValues {
   static const gapW20 = SizedBox(width: 20);
 
   static const gapH5 = SizedBox(height: 5);
+  static const gapH7 = SizedBox(height: 7);
   static const gapH10 = SizedBox(height: 10);
   static const gapH15 = SizedBox(height: 15);
+  static const gapH18 = SizedBox(height: 18);
   static const gapH20 = SizedBox(height: 20);
 }
--- lib/screen/my_page/refund_info_screen.dart
+++ lib/screen/my_page/refund_info_screen.dart
@@ -1,13 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api_ex/config/common/sized_box_values.dart';
 import 'package:rest_api_ex/config/custom_app_bar.dart';
+import 'package:rest_api_ex/design/color_styles.dart';
+import 'package:rest_api_ex/design/font_styles.dart';
 
 class RefundInfoScreen extends StatelessWidget {
   const RefundInfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bankName = TextEditingController(text: '신한은행');
+    final accountHolder = TextEditingController(text: '남정광');
+    final account = TextEditingController(text: '3020735977861');
+
     return Scaffold(
-      appBar: CustomAppBar(title: '환불 정보',),
+      appBar: const CustomAppBar(title: '환불 정보'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(27, 10, 27, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _textField(formName: '은행명', controller: bankName),
+            SizedBoxValues.gapH18,
+            _textField(formName: '예금주명', controller: accountHolder),
+            SizedBoxValues.gapH18,
+            _textField(formName: '계좌번호', controller: account),
+            Padding(
+              padding: const EdgeInsets.only(top: 28),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+                  backgroundColor: AppColors.main,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  '수정하기',
+                  style: FontStyles.Body2.copyWith(color: AppColors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textField({
+    required String formName,
+    required TextEditingController controller,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          formName,
+          style: FontStyles.Body2.copyWith(color: AppColors.black),
+        ),
+        SizedBoxValues.gapH7,
+        TextFormField(
+          controller: controller,
+          style: FontStyles.Body2.copyWith(color: AppColors.black),
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.main),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

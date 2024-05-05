b8290076a7ec159f855e2af00cbd9b9104ce717b
nala0897@naver.com <nala0897@naver.com>
2024-05-05 16:01:06 +0900

    refactor: view 와 viewModel 의존성 분리

--- lib/screen/sign_up/email_auth_check_screen.dart
+++ lib/screen/sign_up/email_auth_check_screen.dart
@@ -3,6 +3,7 @@ import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:provider/provider.dart';
 import 'package:rest_api_ex/config/constants.dart';
 import 'package:rest_api_ex/config/custom_app_bar.dart';
+import 'package:rest_api_ex/config/navigate_to.dart';
 import 'package:rest_api_ex/config/user_info_text_form_field.dart';
 import 'package:rest_api_ex/config/validation_check.dart';
 import 'package:rest_api_ex/data/network/error_handler.dart';
@@ -27,61 +28,80 @@ class EmailAuthCheckScreen extends StatefulWidget {
 }
 
 class _EmailAuthCheckScreenState extends State<EmailAuthCheckScreen> {
-  final formKey = GlobalKey<FormState>();
+  final _emailAuthCodeController = TextEditingController();
+  bool _isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _emailAuthCodeController.addListener(_updateAuthState);
+  }
+
+  @override
+  void dispose() {
+    _emailAuthCodeController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => EmailAuthViewModel(context),
-      builder: (context, child) {
-        final viewModel = context.watch<EmailAuthViewModel>();
+    final viewModel = context.watch<EmailAuthViewModel>();
 
-        return Scaffold(
-          appBar: CustomAppBar(title: widget.appBarTitle),
-          body: ModalProgressHUD(
-            inAsyncCall: viewModel.showSpinner,
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
-              child: Form(
-                key: formKey,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      // 설명 문구,
-                      description(widget._userEmail),
+    return Scaffold(
+      appBar: CustomAppBar(title: widget.appBarTitle),
+      body: ModalProgressHUD(
+        inAsyncCall: viewModel.showSpinner,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Form(
+            key: viewModel.formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // 설명 문구,
+                  description(widget._userEmail),
 
-                      UserInfoTextFormField(
-                        autoFocus: true,
-                        isButtonEnabled:
-                            viewModel.isButtonEnabled == true ? true : false,
-                        textInputType: TextInputType.emailAddress,
-                        controller: viewModel.emailAuthCodeController,
-                        validator: validateAuthCode,
-                        decorationLabelText: '',
-                      ),
-
-                      // 이메일 인증하기 버튼
-                      emailAuthRequestButton(
-                        context,
-                        viewModel,
-                        widget._userEmail,
-                      ),
-
-                      // 이메일 재전송
-                      if (widget.appBarTitle == Constants.signUp)
-                        emailResendButton(viewModel, widget._userEmail),
-                    ],
+                  UserInfoTextFormField(
+                    autoFocus: true,
+                    isButtonEnabled: _isButtonEnabled == true ? true : false,
+                    textInputType: TextInputType.emailAddress,
+                    controller: _emailAuthCodeController,
+                    validator: validateAuthCode,
+                    decorationLabelText: '',
                   ),
-                ),
+
+                  // 이메일 인증하기 버튼
+                  emailAuthRequestButton(
+                    context,
+                    viewModel,
+                    widget._userEmail,
+                  ),
+
+                  // 이메일 재전송
+                  if (widget.appBarTitle == Constants.signUp)
+                    emailResendButton(viewModel, widget._userEmail),
+                ],
               ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
+  }
+
+  // 이메일 인증하기 버튼 활성화
+  void _updateAuthState() {
+    if (validateAuthCode(_emailAuthCodeController.text) == null) {
+      _isButtonEnabled = true;
+    } else {
+      _isButtonEnabled = false;
+    }
+    setState(() {});
   }
 
   // 설명 문구
@@ -115,27 +135,36 @@ class _EmailAuthCheckScreenState extends State<EmailAuthCheckScreen> {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         elevation: 0,
-        side: viewModel.isButtonEnabled
+        side: _isButtonEnabled
             ? const BorderSide(color: AppColors.main)
             : const BorderSide(color: AppColors.gray4),
-        backgroundColor:
-            viewModel.isButtonEnabled ? AppColors.main : AppColors.gray4,
+        backgroundColor: _isButtonEnabled ? AppColors.main : AppColors.gray4,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(4.0),
         ),
       ),
       onPressed: () {
-        if (viewModel.isButtonEnabled == false) {
+        if (_isButtonEnabled == false) {
           return;
         }
 
         try {
-          if( widget.appBarTitle == Constants.signUp ) {
-            viewModel.authCodeSubmit(email, SignUpScreen(userEmail: email));
-          } else {
-            viewModel.authCodeSubmit(email, const ResetPwScreen());
-          }
-
+          viewModel.authCodeSubmit(email, _emailAuthCodeController.text,
+              (result) {
+            if (result == 'true') {
+              if (widget.appBarTitle == Constants.signUp) {
+                navigateTo(context, SignUpScreen(userEmail: email));
+              } else {
+                navigateTo(context, const ResetPwScreen());
+              }
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('인증 코드가 맞지 않습니다.'),
+                ),
+              );
+            }
+          });
         } catch (e) {
           final errorMessage = ErrorHandler.handle(e).failure;
           debugPrint(errorMessage);
@@ -161,6 +190,11 @@ class _EmailAuthCheckScreenState extends State<EmailAuthCheckScreen> {
       ),
       onPressed: () {
         viewModel.emailResend(email);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('이메일이 재전송되었습니다.'),
+          ),
+        );
       },
       child: Text(
         Constants.resendEmail,

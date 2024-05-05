52ffe123689df18a2040ae9e8ce49cecb65577df
nala0897@naver.com <nala0897@naver.com>
2024-05-05 11:44:41 +0900

    feat: EmailRequestViewModel 생성으로 EmailAuthViewModel 역할 분리 및 emailSubmit() 리팩토링

---
+++ lib/screen/view_model/email_request_view_model.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:rest_api_ex/data/network/error_handler.dart';
+import 'package:rest_api_ex/data/source/rest_client.dart';
+
+class EmailRequestViewModel with ChangeNotifier {
+  final RestClient restClient = GetIt.instance<RestClient>();
+  bool _showSpinner = false;
+  String result = '';
+
+  bool get showSpinner => _showSpinner;
+
+  void emailResend(String email) async {
+    _showSpinner = true;
+    notifyListeners();
+
+    try {
+      await restClient.emailAuth(email);
+
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(
+      //     content: Text('이메일이 전송되었습니다.'),
+      //   ),
+      // );
+    } catch (error) {
+      final errorMessage = ErrorHandler.handle(error).failure;
+      debugPrint(errorMessage);
+    }
+
+    _showSpinner = false;
+
+    notifyListeners();
+  }
+
+  void emailSubmit(
+    String appTitle,
+    String email,
+    Function(String) callback,
+  ) async {
+    _showSpinner = true;
+    notifyListeners();
+
+    restClient.emailAuth(email).then((_) {
+      callback('이메일이 전송되었습니다.');
+    }).catchError((error) {
+      final errorMessage = ErrorHandler.handle(error).failure;
+
+      callback(errorMessage);
+      debugPrint('result: $errorMessage');
+    });
+
+    _showSpinner = false;
+
+    notifyListeners();
+  }
+}
